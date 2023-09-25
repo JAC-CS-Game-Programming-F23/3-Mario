a5f42285e4edfd123a2f43a461a692198adbbdcd
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2023-09-24 20:29:00 -0400

    Initial commit.

---
+++ src/Mario-1/src/objects/Tile.js
@@ -0,0 +1,23 @@
+import Vector from "../../lib/Vector.js";
+
+export default class Tile {
+	static SIZE = 16;
+
+	/**
+	 * Represents one tile in the Tilemap and on the screen.
+	 *
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {number} id Index for the sprites array.
+	 * @param {array} sprites All textures this tile can have.
+	 */
+	constructor(x, y, id, sprites) {
+		this.position = new Vector(x, y);
+		this.id = id;
+		this.sprites = sprites;
+	}
+
+	render() {
+		this.sprites[this.id].render(this.position.x * Tile.SIZE, this.position.y * Tile.SIZE);
+	}
+}
---
+++ src/Mario-1/src/states/PlayState.js
@@ -0,0 +1,62 @@
+import Camera from "../../lib/Camera.js";
+import State from "../../lib/State.js";
+import { CANVAS_WIDTH, context } from "../globals.js";
+import LevelMaker from "../services/LevelMaker.js";
+
+export default class PlayState extends State {
+	constructor() {
+		super();
+
+		this.map = LevelMaker.generateLevel();
+		this.camera = new Camera();
+	}
+
+	update(dt) {
+		this.camera.update(dt);
+	}
+
+	render() {
+		this.renderViewport();
+		this.renderGameParameters();
+		this.renderLine();
+	}
+
+	renderViewport() {
+		context.save();
+
+		/**
+		 * Translate scene by camera scroll amount. Negative value shifts have
+		 * the effect of making it seem like we're actually moving right and vice-versa.
+		 *
+		 * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate
+		 */
+		context.translate(-this.camera.position.x, this.camera.position.y);
+
+		this.renderXZero();
+
+		this.map.render();
+		context.restore();
+	}
+
+	renderGameParameters() {
+		context.fillStyle = 'white';
+		context.font = '20px Joystix';
+		context.fillText(`Camera Position: ${this.camera.position.x}`, 10, 25);
+	}
+
+	renderLine() {
+		for (let i = 0; i < 15; i++) {
+			context.fillStyle = 'red';
+			context.fillRect(CANVAS_WIDTH / 2, i * 20, 1, 10);
+		}
+	}
+
+	renderXZero() {
+		context.fillStyle = 'white';
+		context.font = '20px Joystix';
+		context.fillText('x = 0', 10, 220);
+		for (let i = 0; i < 15; i++) {
+			context.fillRect(0, i * 20, 2, 10);
+		}
+	}
+}
